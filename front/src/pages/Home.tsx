b70a060416098d6ec7a422ab3f4ecea4e085fe1b
Aljaž Roglič <aljaz.roglic11@gmail.com>
2024-04-20 14:50:14 +0200

    Added JointJS to project

---
+++ front/src/pages/Home.tsx
@@ -0,0 +1,52 @@
+import React, { useEffect, useRef } from 'react';
+import { dia, shapes, ui } from '@joint/plus';
+import './Home.scss'; // import './App.css'; - if using native CSS
+
+function Home() {
+  const canvas: any = useRef(null);
+
+  useEffect(() => {
+    const graph = new dia.Graph();
+    const paper = new dia.Paper({
+      model: graph,
+      background: {
+        color: '#F8F9FA',
+      },
+      frozen: true,
+      async: true,
+      sorting: dia.Paper.sorting.APPROX,
+      cellViewNamespace: shapes,
+    });
+
+    const scroller = new ui.PaperScroller({
+      paper,
+      autoResizePaper: true,
+      cursor: 'grab',
+    });
+
+    canvas.current.appendChild(scroller.el);
+    scroller.render().center();
+
+    const rect = new shapes.standard.Rectangle({
+      position: { x: 100, y: 100 },
+      size: { width: 100, height: 50 },
+      attrs: {
+        label: {
+          text: 'Hello World',
+        },
+      },
+    });
+
+    graph.addCell(rect);
+    paper.unfreeze();
+
+    return () => {
+      scroller.remove();
+      paper.remove();
+    };
+  }, []);
+
+  return <div className="canvas" ref={canvas} />;
+}
+
+export default Home;
